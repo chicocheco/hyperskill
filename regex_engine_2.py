31e7d2a77ce98c3efb14feb1e9a00a4e62aea72b
Stanislav Matas <stanislav.matas@gmail.com>
2020-10-21 19:22:45 +0200

    refactored

--- regex_engine_2.py
+++ regex_engine_2.py
@@ -1,15 +1,13 @@
 def regex_func(inp: str):
-    regex, string = (list(i) for i in inp.split('|'))
+    regex, string = inp.split('|')
 
     if not regex:  # regex consumed
         return True
     if string:
         if regex[0] == string[0] or regex[0] == '.':
-            return regex_func(''.join(regex[1:]) + '|' + ''.join(string[1:]))
-        else:
-            return False
-    else:
+            return regex_func('|'.join((regex[1:], string[1:])))
         return False
+    return False  # string consumed
 
 
 print(regex_func(input()))
